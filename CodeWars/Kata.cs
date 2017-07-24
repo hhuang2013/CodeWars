02b5c84201fb977cd7d4b38f2fc602534944231e
hhuang2013 <herohest@163.com>
2017-07-24 16:17:02 +0800

    add some solutions

---
+++ CodeWars/Kata.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+public class Kata
+{
+    #region Convert string to camel case
+    public static string ToCamelCase(string str)
+    {
+        string[] tmp = str.Replace('_', '-').Split('-');
+        if (tmp.Length == 1)
+            return str;
+        for (int i = 1; i < tmp.Length;i++ )
+        {
+            tmp[i] = tmp[i].Substring(0, 1).ToUpper() + tmp[i].Substring(1);
+        }
+        return string.Join("",tmp);
+    }
+    #endregion
+
+    #region Gap in Primes
+    public static long[] Gap(int g, long m, long n)
+    {
+        long start, stop = nextPrime(m-1);
+        do
+        {
+            start = stop;
+            stop = nextPrime(start);
+            if (stop > n)
+                return null;
+
+        } while ((stop - start) != g);
+
+        return new long[] { start, stop } ;
+    } 
+    static long nextPrime(long m)
+    {
+        for(long i=m+1;;i++)
+        {
+            if (isPrime(i))
+                return i;
+        }
+    }
+
+    static bool isPrime(long m)
+    {
+        if (m == 2 || m == 3)
+            return true;
+
+        for(long i=2;i<m;i++)
+        {
+            if (m % i == 0)
+                return false;
+            if (i > (m / i))
+                break;
+        }
+        return true;
+    }
+    #endregion
+
+    #region Good vs Evil
+    public static string GoodVsEvil(string good, string evil)
+    {
+        int[] gw = { 1, 2, 3, 3, 5, 10 };
+        int[] bw = { 1, 2, 2, 2, 3, 5, 10 };
+
+        int gsum = 0, bsum = 0;
+        string[] gnum= good.Split(' ');
+        string[] bnum = evil.Split(' ');
+
+        for (int i = 0; i < gnum.Length; i++)
+            gsum += int.Parse(gnum[i]) * gw[i];
+
+        for (int i = 0; i < bnum.Length; i++)
+            bsum += int.Parse(bnum[i]) * bw[i];
+
+        if (gsum > bsum)
+            return "Battle Result: Good triumphs over Evil";
+        else if (gsum < bsum)
+            return "Battle Result: Evil eradicates all trace of Good";
+        else
+            return "Battle Result: No victor on this battle field";
+    }
+
+    #endregion
+
+    #region Car Park Escape
+    public string[] escape(int[,] carpark)
+    {
+        List<string>  rt = new List<string>();
+
+        int curPos;
+        //int stairPos;
+        
+        for(int i=0;i<carpark.GetLength(1);i++)
+        {
+            if (carpark[0,i]==2)
+            {
+                curPos=i;
+                break;
+            }
+        }
+
+         for(int i=0;i<carpark.Length;i++)
+        {
+            
+            if (i<carpark.Length -1)
+            {
+                for (int j = 0; j < carpark.GetLength(1); j++)
+                {
+                    if (carpark[0, i] == 1)
+                    {
+                        curPos = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+        return rt.ToArray();
+    }
+    public int findindex(int[] level,int s)
+    {
+        for(int i=0;i<level.Length;i++)
+        {
+            if (level[i] == s)
+                return i;
+        }
+        return -1;
+    }
+    #endregion
+
+    #region Twice linear
+    public static int DblLinear(int n)
+    {
+         SortedSet<int> lst= new SortedSet<int>() {1};
+        for (int i = 0; i < n; i++)
+        {
+            int first = lst.Min;
+            lst.Add(first * 2 + 1);
+            lst.Add(first * 3 + 1);
+            lst.Remove(first);
+        }
+        return lst.Min;
+    }
+    #endregion
+
+    #region Wave Sorting
+    public static void WaveSort(int[] arr)
+    {
+        List<int> lst = new List<int>(arr);
+        lst.Sort();
+        for (int i = 0; i < arr.Length-1 ; i++)
+        {
+            arr[i] = i % 2 == 0 ? lst[lst.Count -1-i/2] : lst[i / 2];
+        }
+    }
+    #endregion
+}
---
+++ KataTest/KataUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class KataTest
+{
+    [TestMethod]
+    public void KataTests()
+    {
+        //Assert.AreEqual("theStealthWarrior", Kata.ToCamelCase("the_stealth_warrior"), "Kata.ToCamelCase('the_stealth_warrior') did not return correct value");
+        //Assert.AreEqual("TheStealthWarrior", Kata.ToCamelCase("The-Stealth-Warrior"), "Kata.ToCamelCase('The-Stealth-Warrior') did not return correct value");
+
+        //CollectionAssert.AreEqual(new long[] { 101, 103 }, Kata.Gap(2, 100, 110), "2, 100, 110");
+        //CollectionAssert.AreEqual(new long[] { 103, 107 }, Kata.Gap(4, 100, 110), "4, 100, 110");
+        //CollectionAssert.AreEqual(null, Kata.Gap(6, 100, 110), "6, 100, 110");
+        //CollectionAssert.AreEqual(new long[] { 359, 367 }, Kata.Gap(8, 300, 400), "8, 300, 400");
+        //CollectionAssert.AreEqual(new long[] { 337, 347 }, Kata.Gap(10, 300, 400), "10, 300, 400");
+
+        //Assert.AreEqual("Battle Result: Evil eradicates all trace of Good", Kata.GoodVsEvil("1 1 1 1 1 1", "1 1 1 1 1 1 1"));
+        //Assert.AreEqual("Battle Result: Good triumphs over Evil", Kata.GoodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0"));
+        //Assert.AreEqual("Battle Result: No victor on this battle field", Kata.GoodVsEvil("1 0 0 0 0 0", "1 0 0 0 0 0 0"));
+
+        //Assert.AreEqual(Kata.DblLinear(10), 22);
+        //Assert.AreEqual(Kata.DblLinear(20), 57);
+        //Assert.AreEqual(Kata.DblLinear(30), 91);
+        //Assert.AreEqual(Kata.DblLinear(50), 175);
+        //Assert.AreEqual(Kata.DblLinear(10000), 157654);
+        //Assert.AreEqual(Kata.DblLinear(20000), 377625);
+        //Assert.AreEqual(Kata.DblLinear(10000), 157654);
+        //Assert.AreEqual(Kata.DblLinear(20000), 377625);
+
+        var arr = new [] { 1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4 };
+        Kata.WaveSort(arr);
+
+    }
+}
+
